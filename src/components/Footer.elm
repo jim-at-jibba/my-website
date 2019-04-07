911fd080100161d707dd384dac4a1604631f34ed
jim-at-jibba <jim@justjibba.net>
2019-04-07 07:20:06 +0100

    Link to elm website

--- src/components/Footer.elm
+++ src/components/Footer.elm
@@ -1,15 +1,15 @@
 module Components.Footer exposing (viewFooter)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, src, width)
+import Html.Attributes exposing (class, href, src, target, width)
 
 
 viewFooter =
     div [ class "footer" ]
         [ footer []
             [ div [ class "footer-logo" ]
-                [ div [] [ p [] [ text "Lovingly built with" ] ]
-                , div [] [ img [ src "/assets/Elm_logo.svg.png", width 30 ] [] ]
+                [ div [] [ p [] [ text "Proudly built with" ] ]
+                , div [] [ a [ href "https://elm-lang.org/", target "_blank" ] [ img [ src "/assets/Elm_logo.svg.png", width 30 ] [] ] ]
                 ]
             ]
         ]
